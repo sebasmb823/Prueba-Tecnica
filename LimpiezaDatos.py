0389f1839bff98e55201188a570fe1d5d30c3408
Sebastian Muñoz Bernal <sebas.sn24@gmail.com>
2026-07-18 18:20:25 -0500

    Limpieza y estandarizacion del datset transacciones

---
+++ LimpiezaDatos.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+df_transacciones = pd.read_csv("transacciones.csv", sep=";")
+
+print(df_transacciones.head())
+
+print(df_transacciones.isnull().sum())
+
+print(f"Filas duplicadas: {df_transacciones.duplicated().sum()}")
+#6 filas duplicadas
+
+df_transacciones.drop_duplicates(inplace=True)
+print(f"Filas duplicadas despues de la limpieza: {df_transacciones.duplicated().sum()}")
+
+#-------------- Estandarizacion de datos -----------------------
+
+df_transacciones["tipo_producto"] = df_transacciones["tipo_producto"].str.lower()
+
+print("--- Limpieza tipo producto ---")
+print(df_transacciones["tipo_producto"].unique())
+#Limpieza tipo producto
+df_transacciones["tipo_producto"] = df_transacciones["tipo_producto"].replace({
+    "ahorros": "Cuenta de Ahorros",
+    "cuenta de ahorros" : "Cuenta de Ahorros",
+    "cuenta ahorros" : "Cuenta de Ahorros",
+    "cta_ahorros":"Cuenta de Ahorros",
+    "cdt " : "CDT",
+    "cdt" : "CDT",
+    "certificado de deposito" : "CDT",
+    "crédito" : "Crédito",
+    "credito" : "Crédito",
+    "cuenta corriente" : "Cuenta Corriente",
+    "corriente" : "Cuenta Corriente",
+    "cta corriente" : "Cuenta Corriente"
+})
+
+print("----------------------------------------")
+print(df_transacciones["tipo_producto"].unique())
